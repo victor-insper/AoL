3ca9058891d9d661dec20f800277f1ee988e7027
Victor Vasconcellos Borba <VictorVB2@insper.edu.br>
2022-07-21 14:50:35 -0300

    <3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,10 @@
 import { useState } from "react";
 
-function Home() {
-    return(
-        <div>
-            <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
+function Home () {
     return (
         <div>
-            <div>
-                {contador}
-            </div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h1>Eu te amor meu amor {'<'}3</h1>
         </div>
     )
 }
-
 export default Home;
